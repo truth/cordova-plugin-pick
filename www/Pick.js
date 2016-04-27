9bf90c45d2ed436048959ab3ae667ab31341da24
truth <truthwzl@hotmail.com>
2016-04-27 23:48:56 +0800

    仿照cordova-plugin-x-toast编写插件代码

--- www/Pick.js
+++ www/Pick.js
@@ -1,13 +1,22 @@
-
-var exec = require('cordova/exec');
-
 /*
+var exec = require('cordova/exec');
 exports.echo = function(arg0, success, error) {
     exec(success, error, "Pick", "echo", [arg0]);
 };
+module.exports = new Pick();
 */
 function Pick() {}
 Pick.prototype.echo =function(arg0, success, error) {
-    exec(success, error, "Pick", "echo", [arg0]);
+    cordova.exec(success, error, "Pick", "echo", [arg0]);
 };
-module.exports = new Pick();
+
+Pick.install = function () {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+
+  window.plugins.pick = new Pick();
+  return window.plugins.pick;
+};
+
+cordova.addConstructor(Pick.install);
